503dac12521fe61ee35c87cf5448671225d55f53
azizaweb <azizaweb@mail.ru>
2025-01-20 21:12:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// let user = {
+//     name: 'Aziza',
+//     age:19,
+//     isAdmin:true,
+
+// }
+// console.log(user.name)
+// console.log(user)
+
+
+// let user = {
+//         name: 'Aziza',
+//         age:19,
+//        isAdmin:true,
+//       lang:['uzbek','rus', 'eng'] array
+//      }
+//      console.log(user.isAdmin)
+//     console.log(user.lang)
+//     console.log(delete user.age)
+//     console.log(user)
+    
+// const item = {
+//     name: 'John'
+// };
+// item.name = 'Peter Parker' изменить что то внутри строки 
+// console.log(item.name)
+
+// let item = {
+//     name: 'Aziza',
+//     age: '24',
+//     lang: 'rus',
+//     surname: 'Agayeva',
+//     study:'University',
+//     birthday: '29septembre',
+//     live: 'tashkent',
+//     food: 'rice',  
+//     adress: 'yunusabad',
+//     friend:'Yulduz',
+// }
+// let key = prompt ('Что вы хотите узнать')
+// alert(item[key]);
+
+const name = prompt("Введите ваше имя:");
+const spaceId = prompt("Введите ваш Space ID:");
+const spacePassword = prompt("Введите ваш Space Password:");
+
+
+const userInfo = {
+  name:'Aziza' ,
+  ID: '122453',
+  password:'245545',
+};
+
+console.log([userInfo]);
+
+
